89b5b2d1ea7ef09b0a50545e834217447f5d0ac6
Robert Nelson <robertcnelson@gmail.com>
2013-10-17 14:16:20 -0500

    kernel bump: v3.12-rc5
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- version.sh
+++ version.sh
@@ -26,8 +26,8 @@ linaro_toolchain="cortex_gcc_4_8"
 
 #Kernel/Build
 KERNEL_REL=3.12
-KERNEL_TAG=${KERNEL_REL}-rc4
-BUILD=bb.org0.1
+KERNEL_TAG=${KERNEL_REL}-rc5
+BUILD=bb.org0.2
 
 #v3.X-rcX + upto SHA
 #KERNEL_SHA="e6e7fb1ffc875adf2dd36d4a135b8d7addda0aea"
